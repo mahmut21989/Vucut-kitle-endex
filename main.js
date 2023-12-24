35389c3fbc7266a1f2c8630cf39ffcab8332f537
Mahmut Topcu <topcumhmt68@gmail.com>
2023-12-24 15:00:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,115 @@
+
+let user1 = document.getElementById("inp1");
+let user2 = document.getElementById("inp2");
+let paragraf = document.getElementById("product");
+let box1 = document.getElementById("rd1");
+let box2 = document.getElementById("rd2");
+
+function btn1() {
+
+    let user3 = user1.value;
+    let user4 = user2.value;
+    // Aşağıdaki kod İnputun içine eğer herhangi bir değer verilmedi ise alet verir 
+    if (user3.trim() == "" || user4.trim() == "") {
+        alert("Lütfen Boy Kilo değeri giriniz");
+        return; // İşlemi durdur, sonuçları gösterme
+    }
+    const cm = parseFloat(user3);
+    const metre = cm / 100;
+
+    const bill = user4 / (metre * metre);
+    const roundedNumber = bill.toFixed(2);
+    paragraf.innerHTML = roundedNumber;
+    console.log(roundedNumber);
+
+    const femaleWeightGroups = {
+        underweight: 18.50,
+        normal: 25,
+        overweight: 30,
+        obese: 40
+    }
+
+    const maleWeightGroups = {
+        underweight: 19.50,
+        normal: 26,
+        overweight: 31,
+        obese: 41
+    }
+    const isFemale = box1.checked
+    const currentWeightGroups = isFemale ? femaleWeightGroups : maleWeightGroups
+
+
+    if (roundedNumber <= currentWeightGroups.underweight) {
+        conclusion.innerHTML = "Düşük kilolu"
+        conclusion.style.color = "#8a2be2"
+        conclusion.style.fontWeight="bold"
+        const unhealthyFoodSections = document.querySelectorAll(".unhealthyFood");
+        for (const section of unhealthyFoodSections) {
+            section.style.display = "none";
+        }
+
+    } else if (roundedNumber < currentWeightGroups.normal) {
+        conclusion.innerHTML = "Normal kilolu" 
+        conclusion.style.color = "blue"
+        conclusion.style.fontWeight="bold"
+        const unhealthyFoodSections = document.querySelectorAll(".unhealthyFood");
+        for (const section of unhealthyFoodSections) {
+            section.style.display = "none";
+        }
+    
+    } else if (roundedNumber < currentWeightGroups.overweight) {
+        conclusion.innerHTML = "Fazla Kilolu"
+        conclusion.style.color = "#ef9a9a"
+        conclusion.style.fontWeight="bold"
+        const unhealthyFoodSections = document.querySelectorAll(".unhealthyFood");
+        for (const section of unhealthyFoodSections) {
+            section.style.display = "block";
+        }
+
+    } else if (roundedNumber < currentWeightGroups.obese) {
+        conclusion.innerHTML = "Obez Olmuşsun Yeme Artık "
+        conclusion.style.color = "#723f3f"
+        const unhealthyFoodSections = document.querySelectorAll(".unhealthyFood");
+        for (const section of unhealthyFoodSections) {
+            section.style.display = "block";
+        }
+
+    } else {
+
+        conclusion.innerHTML = "Aşırı Obez Yemeeeeeeeee"
+        const unhealthyFoodSections = document.querySelectorAll(".unhealthyFood");
+        conclusion.style.color = "red"
+        for (const section of unhealthyFoodSections) {
+            section.style.display = "block";
+        }
+    }
+
+
+
+}
+
+function female() {
+    // document.getElementsByTagName("body")[0].style.backgroundColor="pink" 
+    // user1.style="background-color:transparent;border:none;border-bottom:1px solid #000;outline:0"
+}
+user1.onfocus = function (a) {
+    console.log(a)
+    a.target.setAttribute("placeholder", "örn: 175")
+    // a.target.style.backgroundColor="#000"
+}
+user2.onfocus = function (a) {
+    console.log(a)
+    a.target.setAttribute("placeholder", "örn: 60")
+    // a.target.style.backgroundColor="#000"
+}
+function male() {
+    // document.getElementsByTagName("body")[0].style.backgroundColor="darkblue" 
+}
+
+
+function btn2() {
+    user1.value = "";
+    user2.value = "";
+    paragraf.innerHTML = "";
+}
+
